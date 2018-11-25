0bf5ddc4011b758abafcd686afc315472d1eb907
Ravindu De Silva <ravindushantha1995@gmail.com>
2018-11-25 13:17:32 +0530

    update completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponentComponent} from './login-component/login-component.component';
 import {HomeComponentComponent} from './home-component/home-component.component';
 import {AdminHeadComponentComponent} from './admin-head-component/admin-head-component.component';
+import {TimeLineCompontentComponent} from './time-line-component/time-line-compontent.component';
 
 const routes: Routes = [
   {path: 'home' , component: HomeComponentComponent},
   {path: 'login', component: LoginComponentComponent},
-  {path: 'test', component: AdminHeadComponentComponent}
+  {path: 'test', component: AdminHeadComponentComponent},
+  {path: 'timeline', component: TimeLineCompontentComponent},
 ];
 
 @NgModule({
