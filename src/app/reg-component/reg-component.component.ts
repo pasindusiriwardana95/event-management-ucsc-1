8f20a3cc82f3ce5df1f64967bd1b83971362d844
pasanjaya <bhadrajeepjaya@gmail.com>
2018-11-28 11:47:08 +0530

    changes

--- src/app/reg-component/reg-component.component.ts
+++ src/app/reg-component/reg-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-reg-component',
@@ -11,71 +12,9 @@ export class RegComponentComponent implements OnInit {
 
   public societyReg: FormGroup;
 
-  constructor(
-    // public servicename: SERVICE,
-    public fb: FormBuilder // Form Builder service for Reactive forms
-  ) { }
+  constructor(public db: AngularFirestore) { }
 
   ngOnInit() {
-    // this.servicename.GetSocList();
-    this.societyForm();
   }
 
-  societyForm() {
-    this.societyReg = this.fb.group({
-      society_name: ['', Validators.required],
-      soc_username: ['', Validators.required],
-      soc_password: ['', Validators.required],
-      soc_description: ['', Validators.required],
-      soc_venue: ['', Validators.required],
-      soc_est_date: ['', Validators.required],
-      soc_ic_name: ['', Validators.required],
-      soc_ic_email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]]
-    });
-  }
-
-
-  // get date using getter
-
-  get socName() {
-    return this.societyReg.get('society_name');
-  }
-
-  get socUsername() {
-    return this.societyReg.get('soc_username');
-  }
-
-  get socPassword() {
-    return this.societyReg.get('soc_password');
-  }
-
-  get socDescription() {
-    return this.societyReg.get('soc_description');
-  }
-
-  get socVenue() {
-    return this.societyReg.get('soc_venue');
-  }
-
-  get socEstDate() {
-    return this.societyReg.get('soc_est_date');
-  }
-
-  get icName() {
-    return this.societyReg.get('soc_ic_name');
-  }
-
-  get icEmail() {
-    return this.societyReg.get('soc_ic_email');
-  }
-  // Reset socity form's values
-  ResetForm() {
-    this.societyReg.reset();
-  }
-  submitSocietyData() {
-    console.log(this.societyReg.value);
-    // this.servicename.AddSociety(this.societyReg.value);
-   // firebase.database().ref().push({'first_name': this.societyReg.value});
-    this.ResetForm();  // Reset form when clicked on reset button
-  }
 }
